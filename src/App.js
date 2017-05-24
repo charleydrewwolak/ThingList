d9d0ba62b3e220e1e6e350b56cdc00301c6afda1
Charley Drew-Wolak <charlzahk@gmail.com>
2017-05-24 09:50:48 -0400

    Add submit button to Davey code :)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header'
+import ThingList from './ThingList'
+import ThingSubmit from './ThingSubmit'
 
 class App extends Component {
+  state = {
+    things: {
+      'thing-1': { id: 'thing-1', name: 'Milk' },
+      'thing-2': { id: 'thing-2', name: 'Bread' },
+      'thing-3': { id: 'thing-3', name: 'Bibb lettuce' },
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <ThingSubmit />
+        <ThingList things={this.state.things} />
       </div>
     );
   }
---
+++ src/ThingSubmit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ThingSubmit.css'
+
+const ThingSubmit = () => {
+  return (
+
+    <button type="submit" className="success ThingSubmit">
+        <i className="fa fa-star"></i>
+    </button>
+  )
+}
+
+export default ThingSubmit
