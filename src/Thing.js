36328943ab9a678b80e72e69f7595a1812b2949f
Charley Drew-Wolak <charlzahk@gmail.com>
2017-05-24 15:39:46 -0400

    Make focus (make me focus better next bcause adhd; help me)

--- src/Thing.js
+++ src/Thing.js
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ContentEditable from 'react-contenteditable'
-import Actions from './Actions'
-import './Thing.css'
 
-const Thing = ({ thing, saveThing, removeThing }) => {
-  const updateName = (ev) => {
+import './Thing.css'
+import Actions from './Actions'
+
+class Thing extends Component {
+  componentDidMount() {
+    this.nameInput.htmlEl.focus()
+  }
+
+  updateName = (ev) => {
+    const { thing, saveThing } = this.props
     thing.name = ev.target.value
     saveThing(thing)
   }
 
-  return (
-    <li className="Thing">
-      <input type="checkbox" value="on" />
-      <div className="details">
-        <ContentEditable
-          className="name"
-          html={thing.name}
-          onChange={updateName}
-        />
-        <Actions removeThing={removeThing} thing={thing} />
-      </div>
-    </li>
-  )
+  blurOnEnter = (ev) => {
+    if (ev.key === 'Enter') {
+      ev.preventDefault()
+      ev.target.blur()
+    }
+  }
+
+  render() {
+    const { thing, removeThing } = this.props
+
+    return (
+      <li className="Thing">
+        <input type="checkbox" value="on" />
+        <div className="details">
+          <ContentEditable
+            className="name"
+            html={thing.name}
+            onChange={this.updateName}
+            onKeyPress={this.blurOnEnter}
+            ref={input => this.nameInput = input}
+          />
+          <Actions thing={thing} removeThing={removeThing} />
+        </div>
+      </li>
+    )
+  }
 }
 
 export default Thing
